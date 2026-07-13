0397b67ab7286f9e48e9f3506810ce7f2472dd36
Amboara <amboaraniainarkt@gmail.com>
2026-07-13 08:39:30 +0300

    feat: add ImageController with POST /images and GET /images

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/ImageController.java
@@ -0,0 +1,34 @@
+package com.example.demo.endpoint.rest.controller;
+
+import com.example.demo.repository.ImageSubmissionRepository;
+import com.example.demo.repository.model.ImageSubmission;
+import com.example.demo.service.ImageService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@AllArgsConstructor
+public class ImageController {
+
+  private final ImageService imageService;
+  private final ImageSubmissionRepository repository;
+
+  @PostMapping("/images")
+  public ResponseEntity<String> uploadImage(
+      @RequestParam("email") String email,
+      @RequestParam("file") MultipartFile file) {
+    imageService.save(file, email);
+    return ResponseEntity.ok("OK");
+  }
+
+  @GetMapping("/images")
+  public List<ImageSubmission> getAllImages() {
+    return repository.findAll();
+  }
+}
